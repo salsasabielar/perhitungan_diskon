10efef6970796c9c4002b9af5358ebf9f1e8eb19
= <=>
2021-03-11 19:43:58 +0700

    membuat fungsi

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  double _inputHarga = 0;
+  double _inputDiskon = 0;
+  double _hasil = 0;
+  double _hemat = 0;
+  final hargaController = TextEditingController();
+  final diskonController = TextEditingController();
+
+  void _hitungDiskon() {
+    setState(() {
+      _inputHarga = double.parse(hargaController.text);
+      _inputDiskon = double.parse(diskonController.text);
+      _hasil = _inputHarga - (_inputHarga * (_inputDiskon / 100));
+      _hemat = _inputHarga - _hasil;
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DISKON',
@@ -33,14 +54,19 @@ class MyApp extends StatelessWidget {
                   children: [
                     Text(
                       "Harga Awal",
-                      style: TextStyle(fontFamily: "times new roman"),
+                      style: TextStyle(
+                          fontFamily: "times new roman", fontSize: 16),
                     ),
                     TextFormField(
+                      controller: hargaController,
                       decoration: InputDecoration(hintText: "0"),
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontFamily: "times new roman", fontSize: 16),
                       keyboardType: TextInputType.numberWithOptions(),
                       validator: (String value) {
                         if (value.isEmpty) {
-                          return 'Enter some Text';
+                          return 'Enter Number';
                         }
                         return null;
                       },
@@ -54,14 +80,17 @@ class MyApp extends StatelessWidget {
                   children: [
                     Text(
                       "Diskon",
-                      style: TextStyle(fontFamily: "times new roman"),
+                      style: TextStyle(
+                          fontFamily: "times new roman", fontSize: 16),
                     ),
                     TextFormField(
+                      controller: diskonController,
                       decoration: InputDecoration(hintText: "0"),
+                      textAlign: TextAlign.end,
                       keyboardType: TextInputType.numberWithOptions(),
                       validator: (String value) {
                         if (value.isEmpty) {
-                          return 'Enter some Text';
+                          return 'Enter Number';
                         }
                         return null;
                       },
@@ -76,13 +105,29 @@ class MyApp extends StatelessWidget {
                   children: [
                     Text(
                       "Harga Akhir",
-                      style: TextStyle(fontFamily: "times new roman"),
+                      style: TextStyle(
+                          fontFamily: "times new roman", fontSize: 16),
                     ),
                     Text(
-                      "Harga Akhir",
-                      style: TextStyle(fontFamily: "times new roman"),
+                      _hasil.toStringAsFixed(0),
+                      style: TextStyle(
+                          fontFamily: "times new roman", fontSize: 16),
                     ),
                   ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {
+                    _hitungDiskon();
+                  },
+                  color: Colors.yellow,
+                  textColor: Colors.black,
+                  child: Text("Enter",
+                      style: TextStyle(
+                          fontSize: 16, fontFamily: "times new roman")),
                 ),
               ),
             ],
